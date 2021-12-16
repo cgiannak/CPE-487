e3025fff4b441d4772185ea585fdd679a93a3937
cgiannak <60272065+cgiannak@users.noreply.github.com>
2021-12-16 15:20:46 -0500

    Update sqrt8_tb.vhdl

--- Assignment_2/sqrt8_tb.vhdl
+++ Assignment_2/sqrt8_tb.vhdl
@@ -19,4 +19,113 @@ architecture test of sqrt8_tb is
   signal x, y, b, u, d, bo : std_ulogic;
 begin
   square_root: sqrt8 port map (x => x, y => y, b => b, u => u, d => d, bo => bo);
+  
+  process begin
+    
+    x <= 'X';
+    y <= 'X';
+    b <= 'X';
+    u <= 'X';
+    wait for 1 ns;
+    
+    x <= '0';
+    y <= '0';
+    b <= '0';
+    u <= '0';
+    wait for 1 ns;
+      
+    x <= '1';
+    y <= '0';
+    b <= '0';
+    u <= '0';
+    wait for 1 ns;
+      
+    x <= '0';
+    y <= '1';
+    b <= '0';
+    u <= '0';
+    wait for 1 ns;    
+      
+    x <= '0';
+    y <= '0';
+    b <= '1';
+    u <= '0';
+    wait for 1 ns;
+    
+    x <= '0';
+    y <= '0';
+    b <= '0';
+    u <= '1';
+    wait for 1 ns;
+      
+    x <= '1';
+    y <= '1';
+    b <= '0';
+    u <= '0';
+    wait for 1 ns;
+      
+    x <= '1';
+    y <= '0';
+    b <= '1';
+    u <= '0';
+    wait for 1 ns;      
+      
+    x <= '1';
+    y <= '0';
+    b <= '0';
+    u <= '1';
+    wait for 1 ns;
+      
+    x <= '0';
+    y <= '1';
+    b <= '1';
+    u <= '0';
+    wait for 1 ns;
+      
+    x <= '0';
+    y <= '1';
+    b <= '0';
+    u <= '1';
+    wait for 1 ns;    
+      
+    x <= '0';
+    y <= '0';
+    b <= '1';
+    u <= '1';
+    wait for 1 ns;
+    
+    x <= '1';
+    y <= '1';
+    b <= '1';
+    u <= '0';
+    wait for 1 ns;
+      
+    x <= '1';
+    y <= '1';
+    b <= '0';
+    u <= '1';
+    wait for 1 ns;
+      
+    x <= '1';
+    y <= '0';
+    b <= '1';
+    u <= '1';
+    wait for 1 ns;     
+      
+    x <= '0';
+    y <= '1';
+    b <= '1';
+    u <= '1';
+    wait for 1 ns;
+      
+    x <= '1';
+    y <= '1';
+    b <= '1';
+    u <= '1';
+    wait for 1 ns;         
+      
+    assert false report "reached end of test";
+    wait;
+  end process;
+    
 end test;
